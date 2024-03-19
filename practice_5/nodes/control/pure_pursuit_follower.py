7df7b776cd80ca3a60784d6644eb98fbcd7ac4b5
Jaak Peterson <peterson.jaak@gmail.com>
2024-03-19 15:45:20 +0200

    Practice 5 solutions.

---
+++ practice_5/nodes/control/pure_pursuit_follower.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import rospy
+import numpy as np
+
+from autoware_msgs.msg import Lane, VehicleCmd
+from geometry_msgs.msg import PoseStamped
+from shapely.geometry import LineString, Point
+from shapely import prepare, distance
+from tf.transformations import euler_from_quaternion
+from scipy.interpolate import interp1d
+
+class PurePursuitFollower:
+    def __init__(self):
+        self.path_linestring = None
+        self.distance_to_velocity_interpolator = None
+
+        # Parameters
+        self.lookahead_distance = rospy.get_param("~lookahead_distance")
+        self.wheel_base = rospy.get_param("/wheel_base")    
+
+        # Publishers
+        self.vehicle_cmd_pub = rospy.Publisher('/control/vehicle_cmd', VehicleCmd, queue_size=10)
+
+        # Subscribers
+        rospy.Subscriber('path', Lane, self.path_callback, queue_size=1)
+        rospy.Subscriber('/localization/current_pose', PoseStamped, self.current_pose_callback, queue_size=1)
+
+    def path_callback(self, msg):
+        # convert waypoints to shapely linestring
+        path_linestring = LineString([(w.pose.pose.position.x, w.pose.pose.position.y) for w in msg.waypoints])
+        waypoints_xy = np.array([(w.pose.pose.position.x, w.pose.pose.position.y) for w in msg.waypoints])
+
+        # prepare path - creates spatial tree, making the spatial queries more efficient
+        prepare(path_linestring)
+        
+        if(len(waypoints_xy) == 0):
+            self.stop_vehicle()
+            return
+        # create distance to velocity interpolator
+        distances = np.cumsum(np.sqrt(np.sum(np.diff(waypoints_xy, axis=0)**2, axis=1)))
+        distances = np.insert(distances, 0, 0)
+        velocities = np.array([w.twist.twist.linear.x for w in msg.waypoints])
+        
+        self.path_linestring = path_linestring
+        self.distance_to_velocity_interpolator = interp1d(distances, velocities, kind='linear', bounds_error=True, fill_value=0.0)
+
+    def current_pose_callback(self, msg):
+        if self.path_linestring is None or self.distance_to_velocity_interpolator is None:
+            return
+        current_pose = Point([msg.pose.position.x, msg.pose.position.y])
+        d_ego_from_path_start = self.path_linestring.project(current_pose)
+
+        lookahead_point = self.path_linestring.interpolate(self.lookahead_distance + d_ego_from_path_start)
+        ld = distance(current_pose, lookahead_point)
+        _, _, heading = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+
+        # lookahead point heading calculation
+        lookahead_heading = np.arctan2(lookahead_point.y - current_pose.y, lookahead_point.x - current_pose.x)
+        heading_diff = lookahead_heading - heading
+        steering_angle = np.arctan((2*self.wheel_base * np.sin(heading_diff))/ld)
+        
+        velocity = self.distance_to_velocity_interpolator(d_ego_from_path_start)
+
+        vehicle_cmd = VehicleCmd()
+        vehicle_cmd.header.stamp = msg.header.stamp
+        vehicle_cmd.header.frame_id = "base_link"
+        vehicle_cmd.ctrl_cmd.steering_angle = steering_angle
+        vehicle_cmd.ctrl_cmd.linear_velocity = velocity
+        self.vehicle_cmd_pub.publish(vehicle_cmd)
+
+    def stop_vehicle(self):
+        vehicle_cmd = VehicleCmd()
+        vehicle_cmd.header.stamp = rospy.Time.now()
+        vehicle_cmd.header.frame_id = "base_link"
+        vehicle_cmd.ctrl_cmd.steering_angle = 0.0
+        vehicle_cmd.ctrl_cmd.linear_velocity = 0.0
+        self.vehicle_cmd_pub.publish(vehicle_cmd)
+        
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('pure_pursuit_follower')
+    node = PurePursuitFollower()
+    node.run()
--- practice_5/nodes/detection/lidar_cluster/cluster_detector.py
+++ practice_5/nodes/detection/lidar_cluster/cluster_detector.py
@@ -30,9 +30,71 @@ class ClusterDetector:
 
         rospy.loginfo("%s - initialized", rospy.get_name())
 
-
     def cluster_callback(self, msg):
-        pass
+        data = numpify(msg)
+        points = structured_to_unstructured(data[['x', 'y', 'z', 'label']], dtype=np.float32)
+        labels = points[:,3].astype(np.int32)
+        # fetch transform for target frame
+        try:
+            transform = self.tf_buffer.lookup_transform(self.output_frame, msg.header.frame_id, msg.header.stamp, rospy.Duration(self.transform_timeout))
+        except (TransformException, rospy.ROSTimeMovedBackwardsException) as e:
+            rospy.logwarn("%s - %s", rospy.get_name(), e)
+            return
+
+        tf_matrix = numpify(transform.transform).astype(np.float32)
+        # make copy of points
+        points = points.copy()
+        # turn into homogeneous coordinates
+        points[:,3] = 1
+        # transform points to target frame
+        points = points.dot(tf_matrix.T)
+
+        header = Header()
+        header.stamp = msg.header.stamp
+        header.frame_id = self.output_frame
+        
+        if points.size == 0:
+            objects_msg = DetectedObjectArray()
+            objects_msg.header = header
+            self.objects_pub.publish(objects_msg)
+            return
+
+        detected_objects = []
+        for i in np.unique(labels): 
+            # create mask
+            mask = (labels == i)
+            points3d = points[mask,:3]
+            if points3d.size < self.min_cluster_size:
+                continue
+
+            object = DetectedObject()
+
+            #calculate centroid
+            centroid = np.mean(points3d, axis=0)
+            object.pose.position.x = centroid[0]
+            object.pose.position.y = centroid[1]
+            object.pose.position.z = centroid[2]
+
+            # create convex hull
+            points_2d = MultiPoint(points[mask,:2])
+            hull = points_2d.convex_hull
+            convex_hull_points = [Point32(x, y, centroid[2]) for x, y in hull.exterior.coords]
+            object.convex_hull.polygon.points = convex_hull_points
+
+            object.id = i
+            object.label = "unknown"
+            object.color = BLUE80P
+            object.valid = True
+            object.space_frame = self.output_frame
+            object.pose_reliable = True
+            object.velocity_reliable = False
+            object.acceleration_reliable = False
+            detected_objects.append(object)
+        
+        objects_msg = DetectedObjectArray()
+        objects_msg.header = header
+        objects_msg.objects = detected_objects
+        self.objects_pub.publish(objects_msg)
 
     def run(self):
         rospy.spin()
